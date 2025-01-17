43199a4b01bccea48170157dfacdb2e5a21c43d1
WILL-29 <77632837+WILL-29@users.noreply.github.com>
2025-01-16 23:01:58 -0400

    Inventario form
    Form inventairo

--- InvenFacil/Form1.Designer.cs
+++ InvenFacil/Form1.Designer.cs
@@ -28,58 +28,266 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.LbSerialNumber = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TbNombreArticulo = new System.Windows.Forms.TextBox();
+            this.lbnombreArtículo = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TbIdArtículo = new System.Windows.Forms.TextBox();
+            this.LbIdArticulo = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox7
             // 
-            this.button1.Location = new System.Drawing.Point(264, 172);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Botón1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.textBox7.Location = new System.Drawing.Point(110, 117);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(130, 20);
+            this.textBox7.TabIndex = 52;
             // 
-            // label1
+            // label7
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(442, 150);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "GRUPO 4";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 124);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 51;
+            this.label7.Text = "Modelo";
             // 
-            // label2
+            // label6
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(445, 211);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Prueba Ray";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "Marca";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(262, 442);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 24);
+            this.button5.TabIndex = 46;
+            this.button5.Text = "Buscar";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(360, 442);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 24);
+            this.button4.TabIndex = 45;
+            this.button4.Text = "Reporte";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(227, 24);
+            this.button3.TabIndex = 44;
+            this.button3.Text = "Seleccionar foto";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 194);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 179);
+            this.pictureBox1.TabIndex = 43;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(110, 169);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 20);
+            this.textBox4.TabIndex = 42;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Cantidad restante";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 20);
+            this.textBox2.TabIndex = 40;
+            // 
+            // LbSerialNumber
+            // 
+            this.LbSerialNumber.AutoSize = true;
+            this.LbSerialNumber.Location = new System.Drawing.Point(10, 150);
+            this.LbSerialNumber.Name = "LbSerialNumber";
+            this.LbSerialNumber.Size = new System.Drawing.Size(33, 13);
+            this.LbSerialNumber.TabIndex = 39;
+            this.LbSerialNumber.Text = "Serial";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Tipo de artículo";
+            // 
+            // TbNombreArticulo
+            // 
+            this.TbNombreArticulo.Location = new System.Drawing.Point(110, 38);
+            this.TbNombreArticulo.Name = "TbNombreArticulo";
+            this.TbNombreArticulo.Size = new System.Drawing.Size(130, 20);
+            this.TbNombreArticulo.TabIndex = 36;
+            // 
+            // lbnombreArtículo
+            // 
+            this.lbnombreArtículo.AutoSize = true;
+            this.lbnombreArtículo.Location = new System.Drawing.Point(8, 45);
+            this.lbnombreArtículo.Name = "lbnombreArtículo";
+            this.lbnombreArtículo.Size = new System.Drawing.Size(100, 13);
+            this.lbnombreArtículo.TabIndex = 35;
+            this.lbnombreArtículo.Text = "Nombre del artículo";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(461, 442);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 24);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(13, 442);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(227, 24);
+            this.button6.TabIndex = 33;
+            this.button6.Text = "Guardar";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(262, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(829, 421);
+            this.dataGridView1.TabIndex = 32;
+            // 
+            // TbIdArtículo
+            // 
+            this.TbIdArtículo.Location = new System.Drawing.Point(110, 12);
+            this.TbIdArtículo.Name = "TbIdArtículo";
+            this.TbIdArtículo.Size = new System.Drawing.Size(130, 20);
+            this.TbIdArtículo.TabIndex = 54;
+            // 
+            // LbIdArticulo
+            // 
+            this.LbIdArticulo.AutoSize = true;
+            this.LbIdArticulo.Location = new System.Drawing.Point(8, 21);
+            this.LbIdArticulo.Name = "LbIdArticulo";
+            this.LbIdArticulo.Size = new System.Drawing.Size(58, 13);
+            this.LbIdArticulo.TabIndex = 53;
+            this.LbIdArticulo.Text = "ID Artículo";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(110, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 21);
+            this.comboBox1.TabIndex = 55;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(110, 90);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 21);
+            this.comboBox2.TabIndex = 56;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1103, 472);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.TbIdArtículo);
+            this.Controls.Add(this.LbIdArticulo);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.LbSerialNumber);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.TbNombreArticulo);
+            this.Controls.Add(this.lbnombreArtículo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "InvenFacil - Inventario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label LbSerialNumber;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TbNombreArticulo;
+        private System.Windows.Forms.Label lbnombreArtículo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TbIdArtículo;
+        private System.Windows.Forms.Label LbIdArticulo;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
